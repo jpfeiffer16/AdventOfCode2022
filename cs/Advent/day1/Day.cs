55c0ca4d0f56b40bd68e34bc9b7eb427627d83ae
jpfeiffer16 <joepfeiffer16@gmail.com>
2022-12-01 21:27:08 -0500

    Day 1

---
+++ cs/Advent/day1/Day.cs
@@ -0,0 +1,45 @@
+/// <summary>
+/// Day1.
+/// </summary>
+public static class Day1 {
+    /// <summary>
+    /// Run.
+    /// </summary>
+    public static void Run() {
+        var i = 0;
+        var file = File.ReadAllLines("./day1/input.txt");
+            // .GroupBy(l => l == string.Empty ? ++i : i)
+            // .ToList()
+            // .Select(g => g.ToList().Where(i => !string.IsNullOrEmpty(i)).ToList()).ToList();
+            //
+        var current = new List<int>();
+        var list = new List<List<int>> { current };
+
+        foreach (var line in file)
+        {
+            if (string.IsNullOrWhiteSpace(line.Trim()))
+            {
+                list.Add(current = new List<int>());
+            }
+            else
+            {
+                current.Add(int.Parse(line.Trim()));
+            }
+        }
+
+        Console.WriteLine("Top 1: {0}", list.Select(g => g.Sum()).Max());
+        Console.WriteLine("Top 3: {0}", list.Select(g => g.Sum()).OrderByDescending(i => i).Take(3).Sum());
+    }
+}
+
+// void PrintList(List<List<int>> list)
+// {
+//     foreach (var group in list)
+//     {
+//         foreach (var count in group)
+//         {
+//             Console.WriteLine(count);
+//         }
+//         Console.WriteLine();
+//     }
+// }
